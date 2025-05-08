ee226f515200ffc74c3b95d4c5f7dbf952d74b09
JinhyeonK <diamondataJH@gmail.com>
2025-05-08 15:31:17 +0900

    Title: 숫자 피라미드 2, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 250508/숫자 피라미드 2/number-pyramid-2.py
+++ 250508/숫자 피라미드 2/number-pyramid-2.py
@@ -1,10 +1,13 @@
-cnt=0
+cnt=1
 n=int(input())
 for i in range(1,n+1):
-    for j in range(1,i):
+    for j in range(1,i+1):
         if j!=i:
-            print(j)
+            print(cnt,end=" ")
             j+=1
+            cnt+=1
         else:
-            print(j)
+            print(cnt)
+            cnt+=1
+
 print()
